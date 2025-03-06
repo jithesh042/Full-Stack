d2079ac173708b905b8abe35f228d319b476ca78
Jithesh <47631880+jithesh042@users.noreply.github.com>
2025-03-06 13:57:25 +0530

    Add files via upload
    Signed-off-by: Jithesh <47631880+jithesh042@users.noreply.github.com>

---
+++ vskg/js/button.js
@@ -0,0 +1,124 @@
+// console.log("AW Dungeon")
+
+// var variablename=variablevalue
+// var a=5;
+// console.log(a);
+// a=10;
+// console.log(a);
+// let c=12;
+// console.log(c);
+// c=8;
+// console.log(c);
+// const s=5;
+// s=18;
+// console.log(s);
+
+
+// var a=3;
+// alert(a);
+// a='hello';
+// alert(a);
+// a='hi';
+// console.log(a)
+
+// let a,b,c;
+// a=5;
+// b=10;
+// c=a+b;
+// console.log(c);
+
+
+// let a="hello",b=3,c=9.5,d=10 - "hii";
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+// let g=9986755226524224724774n;
+// let f=g + 8n;
+// console.log(g);
+
+// let none= null;
+// console.log(none);
+
+// const value1=Symbol("hello");
+// const value2=Symbol("hello");
+// console.log(value1===value2);
+
+// operator
+// arithemtic operator
+// let s = 5;
+// console.log("add:",s + 7);
+// console.log("sub:",s - 6);
+// console.log("mul:",s * 2);
+// console.log("div:",s/5 );
+// console.log("mod:",s%2);
+// console.log("inc:",++s);
+// console.log("dec:",--s);
+// console.log("exp:",s**5);
+
+// assignment oppertor
+// let zz=5;
+// zz+=5;
+// console.log("assign:",zz);
+
+// let za=2, zb=10;
+// console.log("boolean:",za>zb);
+
+// comparision operator
+// let a=10, b=18;
+// console.log("equal to",a==b);
+// console.log("equal to",2=='2');
+// console.log("equal to",a>=b);
+// console.log("equal to",a<=b);
+
+// logical operator
+// let a=5;
+// console.log((a<10)&&(a>3));
+// console.log((a>3)||(a>10));
+// console.log((a==5));
+
+// cnd statements
+// const number=prompt("enter the number");
+// if(number>0){
+//     console.log("number is positive");
+// }
+// else{
+//     console.log("number is negative");
+// }
+
+// const a=prompt("enter the mark");
+// if(a>90){
+//     console.log("grade A");
+// }
+// else if(a>80){
+//     console.log("grade B");
+// }
+// else if(a>70){
+//     console.log("grade c");
+// }
+// else if(a>60){
+//     console.log("grade d");
+// }
+// else if(a>=50){
+//     console.log("grade e");
+// }
+// else {
+//     console.log("fail");
+// }
+
+// looping
+// for (let i = 0; i <= 5; i++) {
+//     console.log(i);
+// }
+
+// task
+let a = prompt("enter the number");
+// let end = prompt("enter the end number");
+for (let i = a; i % 2 == 0; i++) {
+    console.log("even number", a);
+}
+for (let b = a; b % 2 != 0; b++) {
+    console.log("odd number", a);
+}
+
